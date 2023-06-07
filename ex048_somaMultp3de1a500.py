7b7fb23628d8f06acad9aea6685e148956072acb
Stynner <leonardostynner@live.com>
2023-06-07 19:58:42 -0300

    soma de todos os valores que são múltiplos de três entre 1 e 500.

---
+++ ex048_somaMultp3de1a500.py
@@ -0,0 +1,17 @@
+cinza = '\033[;37m'
+azulClaro = '\033[;36m'
+limpa = '\033[m'
+print("""{}
+==================== DESAFIO 046 ======================
+Faça um programa que calcule a soma entre todos os 
+números impares que são múltiplos de três e que se 
+encontram no intervalo de 1 até 500.
+=======================================================\n{}""".format(cinza, limpa))
+contador = 0
+soma = 0
+for cont in range(1,501, 2):
+    if cont % 3 == 0:
+        #print(cont, end=",")
+        contador += 1
+        soma += cont
+print("\nA soma de todos os {}{}{} valores que são múltiplos de três entre 1 e 500 é {}{}{}.".format(azulClaro, contador, limpa, azulClaro, soma, limpa))
